225b8d816c6a5798c8c2e936a63b335d0fc78610
Suryoday18 <suryoday.yadav@transerve.com>
2023-01-05 17:55:21 +0530

    routes files

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const path = require('path')
+
+const router = express.Router()
+
+//admin/add-product
+router.get('/add-product', (req, res, next) => {
+    //res.send('<form action="add-product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>')
+    res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'))
+})
+
+// admin/add-product
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+module.exports = router
